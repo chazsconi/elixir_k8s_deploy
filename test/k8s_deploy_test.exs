deae747e304d64c67520e061232a369316e5c15d
Charles Bernasconi <charles.bernasconi@xing.com>
2022-04-05 09:04:32 +0200

    Fix breaking non-existent tests

--- test/k8s_deploy_test.exs
+++ test/k8s_deploy_test.exs
@@ -1,8 +1,7 @@
 defmodule K8SDeployTest do
   use ExUnit.Case
-  doctest K8SDeploy
 
-  test "greets the world" do
-    assert K8SDeploy.hello() == :world
+  test "the truth" do
+    assert true == true
   end
 end
